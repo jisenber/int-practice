f485874e75ceaa3fefdeb70fd5e3cc696ba2a8ed
jisenber <isenberg.jacob@yahoo.com>
2017-09-11 12:24:37 -0700

    super reduced string problem complete

--- algorithms/strings/super-reduced-string.js
+++ algorithms/strings/super-reduced-string.js
@@ -1,30 +1,65 @@
 /*
 Steve has a string s, consisting of n lowercase letters in one operation, he can delete any pair of adjacent letters with the same value
 
+Sample Case 0
+
+Steve can perform the following sequence of operations to get the final string:
+
+aaabccddd → abccddd
+abccddd → abddd
+abddd → abd
+Thus, we print abd.
+
+Sample Input 1
+
+baab
+Sample Output 1
+
+Empty String
+Explanation 1
+
+Steve can perform the following sequence of operations to get the final string:
+
+baab → bb
+bb → Empty String
+Thus, we print Empty String.
+
+Sample Input 2
+
+aa
+Sample Output 2
+
+Empty String
+
+Strategy:
+base case - if the input is falsy, return 'Empty String'
+iterate over the string, if an element is equal to the adjacent elemment, invoke helper function and break the loop
+if you're at the end of the iteration, return the string
+
+if the for loop is broken, recursively call the function with the new string created by the helper function that takes away the two characters
 
 */
 
 
-function super_reduced_string(s){
-    let stringArr = s.split('')
-    let answer = []
-
-    for (let i = 0; i < s.length + 1; i++) {
-        if(stringArr[0] === stringArr[1]) {
-            stringArr.shift()
-            stringArr.shift()
-        } else {
-            answer.push(stringArr[0])
-            stringArr.shift()
-        }
+function super_reduced_string(s) {
+  let newString = '';
+  if(!s) {
+    return 'Empty String';
+  }
+  for (let i = 0; i < s.length; i++) {
+    if(s[i] === s[i+1]) {
+      newString += stringChange(s, i);
+      break;
+    } else if (!s[i+1]) {
+      return s;
     }
-    if(answer.length) {
-      return answer.join('')
-    } else {
-        return 'Empty String'
-    }
+  }
+  return super_reduced_string(newString);
 }
 
-function reduceMe(string) {
-    if (super_reduced_string(s) !== 'Empty String')
+function stringChange(string, i) {
+  let leftSide = string.slice(0, i);
+  let rightSide = string.slice(i+2);
+  let joinString = leftSide += rightSide;
+  return joinString;
 }
--- algorithms/strings/two-characters.js
+++ algorithms/strings/two-characters.js
@@ -7,5 +7,37 @@ given s, convert it to the longest possible string t. if no string t can be form
 
 strategy:
 
-I'll think about it...
+count all the characters
+pair the ones that are plus or minus 1 of eachother
+check to see if each pair is alternating
+if none are alternating, print 0
 */
+
+let stringMap = {
+  a: 1,
+  b: 5,
+  c: 5,
+  d: 2
+};
+
+
+
+// function charCount(string) {
+//   for (let i = 0; i < string.length; i++) {
+//     if (!stringMap[string[i]]) {
+//       stringMap[string[i]] = 1;
+//     } else {
+//       stringMap[string[i]] +=1;
+//     }
+//   }
+// }
+
+function pairArrays(object) {
+  let newArr = []
+  let keyArr= Object.keys(object)
+  for (let i = 0; i < keyArr.length; i++) {
+    if object[keyArr[i] === ] //working on it...
+  }
+}
+
+pairArrays(stringMap);
